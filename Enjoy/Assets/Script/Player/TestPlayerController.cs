2e4a00756231b34115a0acc0adce8ff9b8f6dd13
Sueyoshi Ryouga <r.sueyosi.humanghm@gmail.com>
2023-12-01 11:01:16 +0900

    [fix] Playerの動き修正中

---
+++ Enjoy/Assets/Script/Player/TestPlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Camera mainCamera;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput =Input.GetAxis("horizontal1");
+        float velocityInput = Input.GetAxis("vertical1");
+        if(horizontalInput != 0.0f || velocityInput != 0.0f)
+        {
+            
+            
+            var input = new Vector3(horizontalInput, 0f, velocityInput) * speed * Time.deltaTime;
+            // // カメラの方向から、X-Z平面の単位ベクトルを取得
+            // Vector3 cameraForward = mainCamera.transform.forward;
+            // Vector3 cameraRight = mainCamera.transform.right;
+            //input = input.normalized;
+            // // Y軸成分を無視する
+            // cameraForward.y = 0f;
+            // cameraRight.y = 0f;
+            // 方向キーの入力値とカメラの向きから、移動方向を決定
+            // Vector3 velocity = input;
+            // //移動ベクトルを正規化する
+            // velocity = velocity.normalized;
+            //  走った場合と歩いた場合
+            //float speed = runFlag ? _model.RunSpeed : _model.WalkSpeed;
+            // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+            //rb.velocity = input * speed + new Vector3(0, rb.velocity.y, 0);
+            transform.Translate(input);
+            //Debug.Log(transform.Translate(input * Time.deltaTime));
+        }
+        
+    }
+}
